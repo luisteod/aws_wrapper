80720a82ce6a9359586ae71f39b2edf813441649
luis henrique <luislopesteo@hotmail.com>
2024-04-19 17:23:13 -0300

    Delete .gitignore and test/aws.ipynb, add tqdm to aws_pandas.py

--- s3/aws_pandas.py
+++ s3/aws_pandas.py
@@ -1,6 +1,7 @@
 import io
 import pandas as pd
 import os
+from tqdm import tqdm
 
 class AwsPandas:
     """
@@ -12,21 +13,21 @@ class AwsPandas:
     def read_folder(self, bucket: str, prefix: str) -> pd.DataFrame:
         df = pd.DataFrame()
         files = self.aws_client.list_files(bucket, prefix)
-        for file in files:
+        for file in tqdm(files, desc="Reading files",leave=False):
             data = self.aws_client.download_file(bucket, file)
             df = pd.concat([df, pd.read_parquet(io.BytesIO(data))])
         return df
     
     
     def save_dataframe(self, bucket: str, prefix: str, filename:str, df: pd.DataFrame) -> None:
-            pq_bin = df.to_parquet(index=False, engine='pyarrow', compression="snappy")
-            path = os.path.join(prefix, f"{filename}.snappy.parquet")
-            self.aws_client.upload_file(pq_bin, bucket, path)
-            part_num += 1
+        pq_bin = df.to_parquet(index=False, engine='pyarrow', compression="snappy")
+        path = os.path.join(prefix, f"{filename}.snappy.parquet")
+        self.aws_client.upload_file(pq_bin, bucket, path)
+        part_num += 1
 
     def save_dataframe_in_chunks(self, bucket: str, prefix: str, df: pd.DataFrame, chunks: int) -> None:
         part_num = 0
-        for i in range(0, len(df), chunks):
+        for i in tqdm(range(0, len(df), chunks), leave=False, desc="Uploading files"):
             start = i 
             if i + chunks >= len(df):
                 end = len(df)
